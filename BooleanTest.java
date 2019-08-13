43e79572c6703dc2ab341612bbd5122b1910b35f
xiongger520 <xiongger520@163.com>
2019-08-13 19:59:10 +0800

    第二个版本

--- BooleanTest.java
+++ BooleanTest.java
@@ -4,11 +4,7 @@ public class BooleanTest
 	{
 		boolean b1 = true;
 		boolean b2 = false;
-		//下面代码将出现错误：字符串不能直接变成boolean型的值
-		//boolean b3 = "true";
-		//使用boolean和字符串进行连接运算，boolean会自动转换成字符串
 		String str = true + "";
-		//下面将输出true
 		System.out.println(str);
 	}
 }
